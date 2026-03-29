2e6e97aff574a6201b136019c5fb5498db878c49
RathodVamshi07 <98532188+Rathodvamshi007@users.noreply.github.com>
2026-03-29 14:03:20 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from environment import Env
+from solver import AStarSolver, BFSSolver, DFSSolver
+
+def get_input():
+    n = int(input())
+    initial = []
+    for i in range(n):
+        row = list(map(int, input().split()))
+        initial.append(row)
+    return initial
