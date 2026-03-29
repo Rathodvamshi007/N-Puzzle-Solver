b340c2a3fdde3b5bcc67532f117eb258e9698cfc
RathodVamshi07 <98532188+Rathodvamshi007@users.noreply.github.com>
2026-03-29 14:24:21 +0530

    main.py
    here it takes the puzzle input, creates the puzzle using Env, and lets the user choose an algorithm (A*, BFS, or DFS) one more we have to add. It then runs the solver and prints the solution steps and number of moves, or shows “No solution” if none is found   i

--- main.py
+++ main.py
@@ -8,3 +8,28 @@ def get_input():
         row = list(map(int, input().split()))
         initial.append(row)
     return initial
+def print_path(path):
+    for step in path:
+        for row in step:
+            print(row)
+        print()
+def main():
+    initial = get_input()
+    puzzle = Env(initial)
+
+    choice = int(input())
+
+    if choice == 1:
+        solver = AStarSolver(puzzle)
+    elif choice == 2:
+        solver = BFSSolver(puzzle)
+    elif choice == 3:
+        solver = DFSSolver(puzzle)
+    else:
+        return
+    path = solver.solve()
+if path:
+        print(len(path) - 1)
+        print_path(path)
+    else:
+        print("No solution")
